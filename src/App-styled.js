810eccc51e16e772824ef0dfc17d8d23656003f2
carlosGabrielCarreno <carrenocarlosgabriel@gmail.com>
2023-03-11 03:06:57 -0300

    working with some components

--- src/App-styled.js
+++ src/App-styled.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const ContainerApp = styled.div`
-  // border: solid red;
+  border: solid blueviole;
   width: 100%;
 
   height: 100vh;
--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,37 +1,52 @@
 import imgRetroOne from "../../../assets/images/image-retro-pcs.jpg";
 import styled from "styled-components";
 
-const FooterContainer = styled.footer``;
+const FooterContainer = styled.footer`
+  border: solid orange;
+  & .footer-card {
+    max-width: 375px;
+    display: flex;
+  }
+  @media screen and (min-width: 800px) {
+    display: flex;
+  }
+`;
 
 export const Footer = () => {
   return (
     <FooterContainer>
       <div className="footer-card">
         <img src={imgRetroOne} alt="" />
-        <h2>01</h2>
-        <h3>Reviving Retro PCs</h3>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam,
-          voluptatum.
-        </p>
+        <div className="footer-data">
+          <h2>01</h2>
+          <h3>Reviving Retro PCs</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam,
+            voluptatum.
+          </p>
+        </div>
       </div>
       <div className="footer-card">
         <img src={imgRetroOne} alt="" />
-        <h2>01</h2>
-        <h3>Reviving Retro PCs</h3>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam,
-          voluptatum.
-        </p>
+        <div className="footer-data">
+          <h2>01</h2>
+          <h3>Reviving Retro PCs</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam,
+            voluptatum.
+          </p>
+        </div>
       </div>
       <div className="footer-card">
         <img src={imgRetroOne} alt="" />
-        <h2>01</h2>
-        <h3>Reviving Retro PCs</h3>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam,
-          voluptatum.
-        </p>
+        <div className="footer-data">
+          <h2>01</h2>
+          <h3>Reviving Retro PCs</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam,
+            voluptatum.
+          </p>
+        </div>
       </div>
     </FooterContainer>
   );
