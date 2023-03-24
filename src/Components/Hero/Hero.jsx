d6ee80ebdea08c04def77116f8aa8ca189adf59b
carlosGabrielCarreno <carrenocarlosgabriel@gmail.com>
2023-03-24 17:34:34 -0300

    add some changes

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -14,17 +14,51 @@ const HeroContainer = styled.div`
       auto
     ); /* Crea 2 filas con altura automática */
     & .img {
-      grid-column: 1 / 2;
+      grid-column: 1 / 3;
       grid-row: 1;
+      max-width: 90%;
+      max-height: 100%;
     }
     & .h1 {
       grid-column: 1;
       grid-row: 2;
+      margin: 0;
+      padding: 0;
     }
-    & .p {
+    & .container-btn-and-text {
       grid-column: 2;
       grid-row: 2;
     }
+    & .ContainerNews {
+      grid-column: 3;
+      grid-row: 1 / 3;
+      color: #fff;
+      height: 100%;
+      border: solid green;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      & .new {
+        background-color: hsl(240, 100%, 5%);
+      }
+    }
+  }
+`;
+
+const ContainerNews = styled.div`
+  /*border: solid gold;
+Primary
+Soft orange: hsl(35, 77%, 62%)
+Soft red: hsl(5, 85%, 63%)
+Neutral
+Off-white: hsl(36, 100%, 99%) 
+Grayish blue: hsl(233, 8%, 79%) 
+Dark grayish blue: hsl(236, 13%, 42%) 
+Very dark blue: hsl(240, 100%, 5%)
+*/
+  color: #fff;
+  & .new {
+    background-color: hsl(240, 100%, 5%);
   }
 `;
 
@@ -38,12 +72,37 @@ export const Hero = () => {
         alt="hero image"
       />
       <h1 className="h1">The Bringht Future of Web 3.0?</h1>
-      <p className="p">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Pariatur odio
-        eaque dolorum! Rem eveniet ad ab excepturi eaque temporibus. Culpa!
-      </p>
-      <button>read more</button>
-      <News />
+      <div className="container-btn-and-text">
+        <p className="p">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Pariatur
+          odio eaque dolorum! Rem eveniet ad ab excepturi eaque temporibus.
+          Culpa!
+        </p>
+        <button>read more</button>
+      </div>
+
+      <div className="ContainerNews">
+        <div className="new">
+          <h2>New</h2>
+          <h3>Hydrogen VS Electric Cars</h3>
+        </div>
+        <div className="new">
+          <h2>The Downsides of Al Artistry</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus
+            repudiandae aliquam harum quos voluptatum autem provident corrupti
+            ab neque id.
+          </p>
+        </div>
+        <div className="new">
+          <h2>Is VC Funding Drying Up</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos ex
+            nisi incidunt suscipit eaque accusamus voluptate, velit architecto
+            quam animi!
+          </p>
+        </div>
+      </div>
     </HeroContainer>
   );
 };
